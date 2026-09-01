4a25ae2e1300ab3cb885449a4175c37f2b466ff6
ABHISHEK KUMAR NISHAD <abhishekkumarnishad1107@gmail.com>
2026-09-01 20:08:56 +0530

    Add configuration file for app settings

---
+++ js/config.js
@@ -0,0 +1,14 @@
+const CONFIG = {
+    // PASTE YOUR GOOGLE APPS SCRIPT WEB APP URL HERE
+    GOOGLE_APPS_SCRIPT_URL: "https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec",
+    
+    // App Settings
+    APP_NAME: "FlatSplit",
+    CURRENCY: "₹",
+    
+    // Default Users (These can be updated in Settings)
+    USERS: {
+        user1: { id: "user1", name: "Person 1", avatar: "👤" },
+        user2: { id: "user2", name: "Person 2", avatar: "👤" }
+    }
+};
